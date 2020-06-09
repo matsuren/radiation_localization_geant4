862122ba722d1ad4fe897cda162075ca2f426c8f
Ren Komatsu <komatsu@robot.t.u-tokyo.ac.jp>
2020-06-09 13:56:09 +0900

    Add CeBr3 detector

--- src/Geometry.cc
+++ src/Geometry.cc
@@ -15,71 +15,91 @@
 #include "G4SystemOfUnits.hh"
 
 //------------------------------------------------------------------------------
-  Geometry::Geometry() {}
+Geometry::Geometry() {}
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-  Geometry::~Geometry() {}
+Geometry::~Geometry() {}
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-  G4VPhysicalVolume* Geometry::Construct()
+G4VPhysicalVolume *Geometry::Construct()
 //------------------------------------------------------------------------------
 {
-  return ConstructDetector();
+    return ConstructDetector();
 }
 
 //------------------------------------------------------------------------------
-  G4VPhysicalVolume* Geometry::ConstructDetector()
+G4VPhysicalVolume *Geometry::ConstructDetector()
 //------------------------------------------------------------------------------
 {
 // Get pointer to 'Material Manager'
-   G4NistManager* materi_Man = G4NistManager::Instance();
+    G4NistManager *materi_Man = G4NistManager::Instance();
 
 // Define 'World Volume'
-   // Define the shape of solid
-   G4double leng_X_World = 1.0*m;         // X-full-length of world
-   G4double leng_Y_World = 1.0*m;         // Y-full-length of world
-   G4double leng_Z_World = 1.0*m;         // Z-full-length of world
-   G4Box* solid_World = 
-     new G4Box("Solid_World", leng_X_World/2.0, leng_Y_World/2.0, leng_Z_World/2.0);
+    // Define the shape of solid
+    G4double leng_X_World = 1.0 * m;         // X-full-length of world
+    G4double leng_Y_World = 1.0 * m;         // Y-full-length of world
+    G4double leng_Z_World = 1.0 * m;         // Z-full-length of world
+    G4Box *solid_World =
+            new G4Box("Solid_World", leng_X_World / 2.0, leng_Y_World / 2.0, leng_Z_World / 2.0);
 
-   // Define logical volume
-   G4Material* materi_World = materi_Man->FindOrBuildMaterial("G4_AIR");
-   G4LogicalVolume* logVol_World = 
-     new G4LogicalVolume(solid_World, materi_World, "LogVol_World");
+    // Define logical volume
+    G4Material *materi_World = materi_Man->FindOrBuildMaterial("G4_AIR");
+    G4LogicalVolume *logVol_World =
+            new G4LogicalVolume(solid_World, materi_World, "LogVol_World");
 //   logVol_World->SetVisAttributes (G4VisAttributes::Invisible);
 
-   // Placement of logical volume
-   G4int copyNum_World = 0;               // Set ID number of world
-   G4PVPlacement* physVol_World  = 
-     new G4PVPlacement(G4Transform3D(), "PhysVol_World", logVol_World, 0, 
-                       false, copyNum_World);
+    // Placement of logical volume
+    G4int copyNum_World = 0;               // Set ID number of world
+    G4PVPlacement *physVol_World =
+            new G4PVPlacement(G4Transform3D(), "PhysVol_World", logVol_World, 0,
+                              false, copyNum_World);
 
-   // Define 'BGO Detector'
-   // Define the shape of solid
-    G4double leng_X_detector = 3.0*cm;
-    G4double leng_Y_detector = 3.0*cm;
-    G4double leng_Z_detector = 3.0*cm;
-    G4Box* solid_BGO = new G4Box("Solid_BGO", leng_X_detector/2.0, leng_Y_detector/2.0, leng_Z_detector/2.0);
+    // Define Detector
+    // Define the shape of solid
+    G4double leng_X_detector = 5.0 * cm;
+    G4double leng_Y_detector = 5.0 * cm;
+    G4double leng_Z_detector = 5.0 * cm;
+    G4Box *solid_detector = new G4Box("Solid_detector", leng_X_detector / 2.0, leng_Y_detector / 2.0,
+                                      leng_Z_detector / 2.0);
 
-   // Define logical volume
-   G4Material* materi_BGO = materi_Man->FindOrBuildMaterial("G4_BGO");
-   G4LogicalVolume* logVol_BGO = 
-     new G4LogicalVolume( solid_BGO, materi_BGO, "LogVol_BGO", 0, 0, 0 );
+    // Define material
+    enum DetectorMat {
+        BGO, CeBr3
+    };
+    const DetectorMat materi_detector_selector = CeBr3;
+    G4Material *materi_detector;
+    switch (materi_detector_selector) {
+        case BGO  :
+            materi_detector = materi_Man->FindOrBuildMaterial("G4_BGO");
+            break;
+        case CeBr3  :
+            G4Element *Br = new G4Element("Bromium", "Br", 35., 79.904 * g / mole);
+            G4Element *Ce = new G4Element("Cerium", "Ce", 58., 140.116 * g / mole);
+            G4Material *CeBr3 = new G4Material("CeBr3", 5.1 * g / cm3, 2);
+            CeBr3->AddElement(Ce, 1);
+            CeBr3->AddElement(Br, 3);
+            materi_detector = CeBr3;
+            break;
+    }
 
-   // Placement of logical volume
-   G4double pos_X_LogV = 0.0*cm;           // X-location LogV 
-   G4double pos_Y_LogV = 0.0*cm;           // Y-location LogV
-   G4double pos_Z_LogV = 0.0*cm;           // Z-location LogV
-   G4ThreeVector threeVect_LogV = G4ThreeVector(pos_X_LogV, pos_Y_LogV, pos_Z_LogV);
-   G4RotationMatrix rotMtrx_LogV = G4RotationMatrix();
-   G4Transform3D trans3D_LogV = G4Transform3D(rotMtrx_LogV, threeVect_LogV);
+    // Define logical volume
+    G4LogicalVolume *logVol_detector =
+            new G4LogicalVolume(solid_detector, materi_detector, "LogVol_detector", 0, 0, 0);
 
-   G4int copyNum_LogV = 1000;                // Set ID number of LogV
-   new G4PVPlacement(trans3D_LogV, "PhysVol_BGO", logVol_BGO, physVol_World, 
-                     false, copyNum_LogV);
+    // Placement of logical volume
+    G4double pos_X_LogV = 0.0 * cm;           // X-location LogV
+    G4double pos_Y_LogV = 0.0 * cm;           // Y-location LogV
+    G4double pos_Z_LogV = 0.0 * cm;           // Z-location LogV
+    G4ThreeVector threeVect_LogV = G4ThreeVector(pos_X_LogV, pos_Y_LogV, pos_Z_LogV);
+    G4RotationMatrix rotMtrx_LogV = G4RotationMatrix();
+    G4Transform3D trans3D_LogV = G4Transform3D(rotMtrx_LogV, threeVect_LogV);
+
+    G4int copyNum_LogV = 1000;                // Set ID number of LogV
+    new G4PVPlacement(trans3D_LogV, "PhysVol_detector", logVol_detector, physVol_World,
+                      false, copyNum_LogV);
 
 // Return the physical world
-   return physVol_World;
+    return physVol_World;
 }
